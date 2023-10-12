0f14ea089b68e254a4479c79198cbe63a5c515a3
tanmaydaga <tanmaydaga06@gmail.com>
2023-10-12 10:25:06 +0530

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,46 @@
+import logo from "../images/logo.svg";
+import { pageLinks, socialLinks } from "../data";
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((pageLink) => {
+            return (
+              <li key={pageLink.id}>
+                <a href={pageLink.href} className="nav-link">
+                  {pageLink.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {socialLinks.map((socialLink) => {
+            return (
+              <li key={socialLink.id}>
+                <a
+                  href={socialLink.href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-icon"
+                >
+                  <i className={socialLink.icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
